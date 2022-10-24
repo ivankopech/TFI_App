eefdfaab53d99ab838a6c0885a166cc5dfdcc4e0
Ivan Kopech <ivankopech@Ivan-2.local>
2022-10-24 18:10:06 -0300

    agrego pantalla de pedidos confirmados

---
+++ lib/screens/pedidos_confirmados_screen.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/orders.dart' show Orders;
+import '../widgets/order_item.dart';
+import '../widgets/another_order_item.dart';
+import '../widgets/app_drawer.dart';
+
+class PedidosConfirmadosScreen extends StatefulWidget {
+  static const routeName = '/pedidos-confirmados';
+
+  @override
+  State<PedidosConfirmadosScreen> createState() =>
+      _PedidosConfirmadosScreenState();
+}
+
+class _PedidosConfirmadosScreenState extends State<PedidosConfirmadosScreen> {
+  Future _ordersFuture;
+
+  Future _obtainOrdersFuture() {
+    return Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
+  }
+
+  @override
+  void initState() {
+    _ordersFuture = _obtainOrdersFuture();
+    super.initState();
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Pedidos')),
+      drawer: AppDrawer(),
+      body: FutureBuilder(
+        future: _ordersFuture,
+        // ignore: missing_return
+        builder: (ctx, dataSnapshot) {
+          if (dataSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else {
+            if (dataSnapshot.error != null) {
+              // do error handling
+              return Center(
+                child: Text('an error ocurred'),
+              );
+            } else {
+              return Consumer<Orders>(
+                builder: (ctx, orderData, child) => ListView.builder(
+                  itemCount: orderData.orders.length,
+                  itemBuilder: (ctx, index) =>
+                      OrderItem(orderData.orders[index], orderData),
+                ),
+              );
+            }
+          }
+        },
+      ),
+    );
+  }
+}
